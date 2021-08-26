5b7d4f165647603250c4844c5ebd15cde1ed97b5
arbaazmakandar <30730654+arbaazmakandar@users.noreply.github.com>
2021-08-26 07:36:56 +0530

    Initial commit

---
+++ src/components/circle.js
@@ -0,0 +1,12 @@
+import Shape from "./shape";
+
+// import
+
+class Circle extends Shape {
+  constructor(color) {
+    super(color);
+  }
+  calculateArea() {}
+}
+
+export default Circle;
---
+++ src/components/shape.js
@@ -0,0 +1,9 @@
+export default class Shape {
+  constructor(color) {
+    this.color = color;
+  }
+
+  drawShape = function () {};
+
+  calculateArea = function () {};
+}
